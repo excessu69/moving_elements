8d501a81cf8d68f14dc8b982682b6d7c2758708c
pasynkov <pasynkov_93@bk.ru>
2025-12-29 02:28:49 +0700

    adding a game stop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./css/style.css";
-
 import Game from "./js/app";
 
 const game = new Game();
 game.init();
+
+window.addEventListener("beforeunload", () => game.stop());
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) game.stop();
+});
